afbf9612a1039554babe9932313aab7833205ef3
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-21 14:08:34 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,3 +1,9 @@
-CREATE TABLE projects ( id INTEGER PRIMARY KEY,
+CREATE TABLE projects ( 
+id INTEGER PRIMARY KEY,
+title TEXT,
+category TEXT,
+funding_goal INTEGER,
+start_date INTEGER,
+end_date INTEGER
 
 )
