a2184a97f086ead14b336c355f02c59a7708b510
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-21 14:11:06 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -5,5 +5,16 @@ category TEXT,
 funding_goal INTEGER,
 start_date INTEGER,
 end_date INTEGER
-)
+);
 
+CREATE TABLE users (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER
+);
+
+
+CREATE TABLE pledges (
+id INTEGER PRIMARY KEY,
+amount INTEGER
+);
